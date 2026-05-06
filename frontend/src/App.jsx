6f6c8c14fb4d5ab8f16d82c5b05a0f771f11c629
Aamir Khan <aamirkhan2671@gmail.com>
2026-05-06 19:44:55 +0530

    navbar is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,11 @@ import Home from "./components/Home";
 import Auth from "./components/Auth";
 import { useEffect } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUserData } from "./redux/userSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -13,8 +16,10 @@ const App = () => {
           "http://localhost:3000/api/v1/user/current-user",
           { withCredentials: true },
         );
+        dispatch(setUserData(result.data));
         console.log(result.data);
       } catch (error) {
+        dispatch(setUserData(null));
         console.log(error);
       }
     };
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="min-h-screen bg-[#f3f3f3] flex flex-col">
+        <Navbar />
+      </div>
+    </>
+  );
 };
 
 export default Home;
